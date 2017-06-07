7b5849662e3180c97c30441700a481be69350c62
Jakob <schuster.jakob@campus.lmu.de>
2017-06-07 22:20:29 +0200

    Aufgabe 3 fertiggestellt

--- Aufgabe3/src/Aufgabe_3/Aufgabe_3.java
+++ Aufgabe3/src/Aufgabe_3/Aufgabe_3.java
@@ -1,19 +1,25 @@
 package Aufgabe_3;
 
+import java.awt.geom.Point2D;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-
-import javafx.collections.FXCollections;
-import javafx.collections.transformation.SortedList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 
 class Aufgabe3 {
 
 	private static final String FILE1 = "data\\s_1000_1.dat";
 	private static final String FILE2 = "data\\s_1000_10.dat";
+	private static final String FILE3 = "data\\s_10000_1.dat";
+	private static final String FILE4 = "data\\s_100000_1.dat";
 	public static ArrayList<double[]> data;
+	public static TreeSet<Integer> intersectionsChecked = new TreeSet<Integer>();
+	public static int numCrossedLines;
 	
 	private enum EventType {
 		Startpoint, Endpoint, Intersection
@@ -62,59 +68,87 @@ class Aufgabe3 {
 		return data;
 	}
 
-	private static SortedList<EventPoint> generateEventQueue(ArrayList<double[]> data) {
-		SortedList<EventPoint> eventQueue = new SortedList<EventPoint>(FXCollections.observableArrayList());
+	private static TreeMap<double[], EventPoint> generateEventQueue(ArrayList<double[]> data) {
+		TreeMap<double[], EventPoint> eventQueue = new TreeMap<double[], EventPoint>(new Comparator<double[]>()
+	    {
+	        public int compare(double[] o1, double[] o2)
+	        {
+    			if (o1[0] == o2[0]) {
+    				if (o1[1] == o2[1]) {
+    					if (o1[2] == o2[2]) {
+            				return -1;
+            			} else {
+            				return ((Double) o1[2]).compareTo((Double) o2[2]);
+            			}
+        			} else {
+        				return ((Double) o1[1]).compareTo((Double) o2[1]);
+        			}
+    			} else {
+    				return ((Double) o1[0]).compareTo((Double) o2[0]);
+    			} 
+	        } 
+	    });
 		
 		for (int i = 0; i < data.size(); i++) {
-			EventPoint event = new EventPoint();
-			event.x = data.get(i)[0];
-			event.y = data.get(i)[1];
-			event.type = EventType.Startpoint;
-			event.seg1_Data_Index = i;
-			addToSortedList(event, eventQueue);
-			
-			event.x = data.get(i)[2];
-			event.y = data.get(i)[3];
-			event.type = EventType.Endpoint;
-			addToSortedList(event, eventQueue);
+			EventPoint event_Startpoint = new EventPoint();
+			event_Startpoint.x = data.get(i)[0];
+			event_Startpoint.y = data.get(i)[1];
+			event_Startpoint.type = EventType.Startpoint;
+			event_Startpoint.seg1_Data_Index = i;
+			addToEventQueue(event_Startpoint, eventQueue);
+
+			EventPoint event_Endpoint = new EventPoint();
+			event_Endpoint.x = data.get(i)[2];
+			event_Endpoint.y = data.get(i)[3];
+			event_Endpoint.type = EventType.Endpoint;
+			event_Endpoint.seg1_Data_Index = i;
+			addToEventQueue(event_Endpoint, eventQueue);
 		}
 		return eventQueue;
 	}
-	
-	public static void addToSortedList(EventPoint event, SortedList<EventPoint> eventQueue) {
-		if (event.type == EventType.Intersection && event.seg1_Data_Index < event.seg2_Data_Index) {
-			int temp = event.seg1_Data_Index;
-			event.seg1_Data_Index = event.seg2_Data_Index;
-			event.seg2_Data_Index = temp;
-		}
-		if (eventQueue.size() == 0) {
-			eventQueue.add(0, event);
-		}
-		for (int i = 0; i < eventQueue.size(); i++) {
-			EventPoint queue_Event = eventQueue.get(i);
-			if (event.x < queue_Event.x) {
-				eventQueue.add(i, event);
-				break;
-			} else if (event.x == queue_Event.x && event.y < queue_Event.y) {
-				eventQueue.add(i, event);
-				break;
-			} else if (event.x == queue_Event.x && event.y == queue_Event.y) {
-				if (event.type != EventType.Intersection || queue_Event.type != EventType.Intersection
-						|| event.seg1_Data_Index != queue_Event.seg1_Data_Index || event.seg2_Data_Index != queue_Event.seg2_Data_Index) {
-					eventQueue.add(i, event);
-				}
-				break;
+
+	public static void addToEventQueue(EventPoint event, TreeMap<double[], EventPoint> eventQueue) {
+		if (event.type == EventType.Intersection) {
+			if (event.seg1_Data_Index < event.seg2_Data_Index) {
+				int temp = event.seg1_Data_Index;
+				event.seg1_Data_Index = event.seg2_Data_Index;
+				event.seg2_Data_Index = temp;
 			}
+
+			if (intersectionsChecked.contains(event.seg1_Data_Index * data.size() + event.seg2_Data_Index)) {
+				return;
+			}
+			intersectionsChecked.add(event.seg1_Data_Index * data.size() + event.seg2_Data_Index);
 		}
+
+		double eventType_Double = 0;
+		switch (event.type) {
+			case Startpoint:
+				eventType_Double = 1;
+				break;
+			case Endpoint:
+				eventType_Double = 3;
+				break;
+			case Intersection:
+				eventType_Double = 2;
+			default:
+				break;
+				
+		}
+		eventQueue.put(new double[] {event.x, event.y, eventType_Double}, event);
 	}
-	
-	public static int addToSweepLine(int seg_Data_Index, SortedList<Integer> sweepLine) {
+
+	public static int addToSweepLine(int seg_Data_Index, LinkedList<Integer> sweepLine) {
 		double[] newSeg = data.get(seg_Data_Index);
 		
+		if (sweepLine.size() == 0) {
+			sweepLine.add(seg_Data_Index);
+			return 0;
+		}
 		for (int i = 0; i < sweepLine.size(); i++) {
 			double[] compSeg = data.get(sweepLine.get(i));
 			double compSeg_y = (newSeg[0] - compSeg[0]) / (compSeg[2] - compSeg[0]) * (compSeg[3] - compSeg[1]) + compSeg[1];
-			
+
 			if (newSeg[1] < compSeg_y) {
 				sweepLine.add(i, seg_Data_Index);
 				return i;
@@ -128,15 +162,16 @@ class Aufgabe3 {
 
 				if (y1 >= y2) {
 					sweepLine.add(i, seg_Data_Index);
+					return i;
 				} else {
 					sweepLine.add(i + 1, seg_Data_Index);
+					return i + 1;
 				}
-
-				return i;
 			}
 		}
 
-		return -1;
+		sweepLine.add(seg_Data_Index);
+		return sweepLine.size() - 1;
 	}
 
 	private static double calcCcw(double p1_x, double p1_y, double p2_x, double p2_y, double p3_x, double p3_y) {
@@ -145,22 +180,18 @@ class Aufgabe3 {
 	}
 
 	private static boolean intersectionTest(int seg1_Data_Index, int seg2_Data_Index) {
-		double i1_x = data.get(seg1_Data_Index)[0];
-		double i1_y = data.get(seg1_Data_Index)[1];
-		double i2_x = data.get(seg1_Data_Index)[2];
-		double i2_y = data.get(seg1_Data_Index)[3];
-		double j1_x = data.get(seg2_Data_Index)[0];
-		double j1_y = data.get(seg2_Data_Index)[1];
-		double j2_x = data.get(seg2_Data_Index)[2];
-		double j2_y = data.get(seg2_Data_Index)[3];
-		double ccw1 = calcCcw(i1_x, i1_y, i2_x, i2_y, j1_x, j1_y);
-		double ccw2 = calcCcw(i1_x, i1_y, i2_x, i2_y, j2_x, j2_y);
-		double ccw3 = calcCcw(j1_x, j1_y, j2_x, j2_y, i1_x, i1_y);
-		double ccw4 = calcCcw(j1_x, j1_y, j2_x, j2_y, i2_x, i2_y);
+		Point2D.Double i1 = new Point2D.Double(data.get(seg1_Data_Index)[0], data.get(seg1_Data_Index)[1]);
+		Point2D.Double i2 = new Point2D.Double(data.get(seg1_Data_Index)[2], data.get(seg1_Data_Index)[3]);
+		Point2D.Double j1 = new Point2D.Double(data.get(seg2_Data_Index)[0], data.get(seg2_Data_Index)[1]);
+		Point2D.Double j2 = new Point2D.Double(data.get(seg2_Data_Index)[2], data.get(seg2_Data_Index)[3]);
+		double ccw1 = calcCcw(i1.x, i1.y, i2.x, i2.y, j1.x, j1.y);
+		double ccw2 = calcCcw(i1.x, i1.y, i2.x, i2.y, j2.x, j2.y);
+		double ccw3 = calcCcw(j1.x, j1.y, j2.x, j2.y, i1.x, i1.y);
+		double ccw4 = calcCcw(j1.x, j1.y, j2.x, j2.y, i2.x, i2.y);
 		if (ccw1 == 0 && ccw2 == 0) {
-			double lambda1 = (j1_x - i1_x) / (i2_x - i1_x);
-			double lambda2 = (j2_x - i1_x) / (i2_x - i1_x);
-			if (i1_x == j1_x || i1_x == j2_x || i2_x == j1_x || i2_x == j2_x) {
+			double lambda1 = (j1.x - i1.x) / (i2.x - i1.x);
+			double lambda2 = (j2.x - i1.x) / (i2.x - i1.x);
+			if (i1.x == j1.x || i1.x == j2.x || i2.x == j1.x || i2.x == j2.x) {
 				return true;
 			} else if ((0 <= lambda1 && lambda1 <= 1) || (0 <= lambda2 && lambda2 <= 1)) {
 				return true;
@@ -173,140 +204,172 @@ class Aufgabe3 {
 	}
 	
 	public static double[] intersection(int seg1_Data_Index, int seg2_Data_Index) {
-		double i1_x = data.get(seg1_Data_Index)[0];
-		double i1_y = data.get(seg1_Data_Index)[1];
-		double i2_x = data.get(seg1_Data_Index)[2];
-		double i2_y = data.get(seg1_Data_Index)[3];
-		double j1_x = data.get(seg2_Data_Index)[0];
-		double j1_y = data.get(seg2_Data_Index)[1];
-		double j2_x = data.get(seg2_Data_Index)[2];
-		double j2_y = data.get(seg2_Data_Index)[3];
-		double lambda_denominator = ((i2_x - i1_x) * (j2_y - j1_y) - (i2_y - i1_y) * (j2_x - j1_x));
+		Point2D.Double i1 = new Point2D.Double(data.get(seg1_Data_Index)[0], data.get(seg1_Data_Index)[1]);
+		Point2D.Double i2 = new Point2D.Double(data.get(seg1_Data_Index)[2], data.get(seg1_Data_Index)[3]);
+		Point2D.Double j1 = new Point2D.Double(data.get(seg2_Data_Index)[0], data.get(seg2_Data_Index)[1]);
+		Point2D.Double j2 = new Point2D.Double(data.get(seg2_Data_Index)[2], data.get(seg2_Data_Index)[3]);
+		double lambda_denominator = ((i2.x - i1.x) * (j2.y - j1.y) - (i2.y - i1.y) * (j2.x - j1.x));
 		if (lambda_denominator == 0) {
-			double[] intersection = {i1_x, i1_y};
+			double[] intersection = {i1.x, i1.y};
 			return intersection;
 		} else {
-			double lambda = ((j1_x - i1_x) * (j2_y - j1_y) - (j1_y - i1_y) * (j2_x - j1_x)) / lambda_denominator;
-			double[] intersection = {i1_x + lambda * (i2_x - i1_x), i1_y + lambda * (i2_y - i1_y)};
+			double lambda = ((j1.x - i1.x) * (j2.y - j1.y) - (j1.y - i1.y) * (j2.x - j1.x)) / lambda_denominator;
+			double[] intersection = {i1.x + lambda * (i2.x - i1.x), i1.y + lambda * (i2.y - i1.y)};
 			return intersection;
 		}
 	}
 	
-	public static void treatStartpoint(EventPoint event, SortedList<Integer> sweepLine, SortedList<EventPoint> eventQueue) {
+	public static void treatStartpoint(EventPoint event, LinkedList<Integer> sweepLine, TreeMap<double[], EventPoint> eventQueue) {
 		int seg_SweepLine_Index = addToSweepLine(event.seg1_Data_Index, sweepLine);
 		if (seg_SweepLine_Index > 0) {
-			if (intersectionTest(seg_SweepLine_Index, seg_SweepLine_Index - 1)) {
+			if (intersectionTest(sweepLine.get(seg_SweepLine_Index), sweepLine.get(seg_SweepLine_Index - 1))) {
 				EventPoint intersectionEvent = new EventPoint();
-				double [] intersection = intersection(seg_SweepLine_Index, seg_SweepLine_Index - 1);
+				double[] intersection = intersection(sweepLine.get(seg_SweepLine_Index), sweepLine.get(seg_SweepLine_Index - 1));
 				intersectionEvent.x = intersection[0];
 				intersectionEvent.y = intersection[1];
 				intersectionEvent.type = EventType.Intersection;
 				intersectionEvent.seg1_Data_Index = sweepLine.get(seg_SweepLine_Index);
 				intersectionEvent.seg2_Data_Index = sweepLine.get(seg_SweepLine_Index - 1);
-				addToSortedList(intersectionEvent, eventQueue);
+				addToEventQueue(intersectionEvent, eventQueue);
 			}
 		}
 		if (seg_SweepLine_Index < sweepLine.size() - 1) {
-			if (intersectionTest(seg_SweepLine_Index, seg_SweepLine_Index + 1)) {
+			if (intersectionTest(sweepLine.get(seg_SweepLine_Index), sweepLine.get(seg_SweepLine_Index + 1))) {
 				EventPoint intersectionEvent = new EventPoint();
-				double [] intersection = intersection(seg_SweepLine_Index, seg_SweepLine_Index + 1);
+				double[] intersection = intersection(sweepLine.get(seg_SweepLine_Index), sweepLine.get(seg_SweepLine_Index + 1));
 				intersectionEvent.x = intersection[0];
 				intersectionEvent.y = intersection[1];
 				intersectionEvent.type = EventType.Intersection;
 				intersectionEvent.seg1_Data_Index = sweepLine.get(seg_SweepLine_Index);
 				intersectionEvent.seg2_Data_Index = sweepLine.get(seg_SweepLine_Index + 1);
-				addToSortedList(intersectionEvent, eventQueue);
+				addToEventQueue(intersectionEvent, eventQueue);
 			}
 		}
 	}
 	
-	public static void treatEndpoint(EventPoint event, SortedList<Integer> sweepLine, SortedList<EventPoint> eventQueue) {
+	public static void treatEndpoint(EventPoint event, LinkedList<Integer> sweepLine, TreeMap<double[], EventPoint> eventQueue) {
 		int seg_SweepLine_Index = -1;
 		for (int i = 0; i < sweepLine.size(); i++) {
 			if (event.seg1_Data_Index == sweepLine.get(i)) {
-				seg_SweepLine_Index = sweepLine.get(i);
+				seg_SweepLine_Index = i;
 			}
 		}
 
 		if (seg_SweepLine_Index > 0 && seg_SweepLine_Index < sweepLine.size() - 1) {
 			if (intersectionTest(sweepLine.get(seg_SweepLine_Index - 1), sweepLine.get(seg_SweepLine_Index + 1))) {
 				EventPoint intersectionEvent = new EventPoint();
-				double [] intersection = intersection(sweepLine.get(seg_SweepLine_Index - 1), sweepLine.get(seg_SweepLine_Index + 1));
+				double[] intersection = intersection(sweepLine.get(seg_SweepLine_Index - 1), sweepLine.get(seg_SweepLine_Index + 1));
 				intersectionEvent.x = intersection[0];
 				intersectionEvent.y = intersection[1];
 				intersectionEvent.type = EventType.Intersection;
 				intersectionEvent.seg1_Data_Index = sweepLine.get(seg_SweepLine_Index - 1);
 				intersectionEvent.seg2_Data_Index = sweepLine.get(seg_SweepLine_Index + 1);
-				addToSortedList(intersectionEvent, eventQueue);
+				addToEventQueue(intersectionEvent, eventQueue);
 			}
-			sweepLine.remove(seg_SweepLine_Index);
 		}
+		sweepLine.remove(seg_SweepLine_Index);
 	}
-	
-	public static void treatIntersection(EventPoint event, SortedList<Integer> sweepLine, SortedList<EventPoint> eventQueue, int numCrossedLines) {
+
+	public static void treatIntersection(EventPoint event, LinkedList<Integer> sweepLine, TreeMap<double[], EventPoint> eventQueue) {
 		numCrossedLines++;
 		int seg1_SweepLine_Index = -1, seg2_SweepLine_Index = -1;
 		for (int i = 0; i < sweepLine.size(); i++) {
 			if (event.seg1_Data_Index == sweepLine.get(i)) {
-				seg1_SweepLine_Index = sweepLine.get(i);
+				seg1_SweepLine_Index = i;
 				if (seg2_SweepLine_Index != -1) break;
 			} else if (event.seg2_Data_Index == sweepLine.get(i)) {
-				seg2_SweepLine_Index = sweepLine.get(i);
+				seg2_SweepLine_Index = i;
 				if (seg1_SweepLine_Index != -1) break;
 			}
 		}
-		
+
 		sweepLine.set(seg1_SweepLine_Index, event.seg2_Data_Index);
 		sweepLine.set(seg2_SweepLine_Index, event.seg1_Data_Index);
+
+		if (seg1_SweepLine_Index > seg2_SweepLine_Index) {
+			int temp = seg1_SweepLine_Index;
+			seg1_SweepLine_Index = seg2_SweepLine_Index;
+			seg2_SweepLine_Index = temp;
+		}
 
 		if (seg1_SweepLine_Index > 0) {
 			if (intersectionTest(sweepLine.get(seg1_SweepLine_Index), sweepLine.get(seg1_SweepLine_Index - 1))) {
 				EventPoint intersectionEvent = new EventPoint();
-				double [] intersection = intersection(sweepLine.get(seg1_SweepLine_Index), sweepLine.get(seg1_SweepLine_Index - 1));
+				double[] intersection = intersection(sweepLine.get(seg1_SweepLine_Index), sweepLine.get(seg1_SweepLine_Index - 1));
 				intersectionEvent.x = intersection[0];
 				intersectionEvent.y = intersection[1];
 				intersectionEvent.type = EventType.Intersection;
 				intersectionEvent.seg1_Data_Index = sweepLine.get(seg1_SweepLine_Index);
 				intersectionEvent.seg2_Data_Index = sweepLine.get(seg1_SweepLine_Index - 1);
-				addToSortedList(intersectionEvent, eventQueue);
+				addToEventQueue(intersectionEvent, eventQueue);
 			}
 		}
 		if (seg2_SweepLine_Index < sweepLine.size() - 1) {
 			if (intersectionTest(sweepLine.get(seg2_SweepLine_Index), sweepLine.get(seg2_SweepLine_Index + 1))) {
 				EventPoint intersectionEvent = new EventPoint();
-				double [] intersection = intersection(sweepLine.get(seg2_SweepLine_Index), sweepLine.get(seg2_SweepLine_Index + 1));
+				double[] intersection = intersection(sweepLine.get(seg2_SweepLine_Index), sweepLine.get(seg2_SweepLine_Index + 1));
 				intersectionEvent.x = intersection[0];
 				intersectionEvent.y = intersection[1];
 				intersectionEvent.type = EventType.Intersection;
 				intersectionEvent.seg1_Data_Index = sweepLine.get(seg2_SweepLine_Index);
 				intersectionEvent.seg2_Data_Index = sweepLine.get(seg2_SweepLine_Index + 1);
-				addToSortedList(intersectionEvent, eventQueue);
+				addToEventQueue(intersectionEvent, eventQueue);
 			}
 		}
 	}
 
-	public static void main(String[] args) throws IOException {
-		long timeStart = System.currentTimeMillis();
-		ArrayList<double[]> data = parseFile(FILE1);
-		SortedList<EventPoint> eventQueue = generateEventQueue(data);
-		SortedList<Integer> sweepLine = new SortedList<Integer>(FXCollections.observableArrayList()); 
-		int numCrossedLines = 0;
-		
+	public static int countCrossedLines(String datafile) throws IOException {
+		numCrossedLines = 0;
+
+		data = parseFile(datafile);
+		TreeMap<double[], EventPoint> eventQueue = generateEventQueue(data);
+		LinkedList<Integer> sweepLine = new LinkedList<Integer>(); 
+
 		while (!eventQueue.isEmpty()) {
-			System.out.println(1);
-			EventPoint event = eventQueue.get(0);
-			if (event.type == EventType.Startpoint) {
-				treatStartpoint(event, sweepLine, eventQueue);
-			} else if (event.type == EventType.Endpoint) {
-				treatEndpoint(event, sweepLine, eventQueue);
-			} else if (event.type == EventType.Intersection) {
-				treatIntersection(event, sweepLine, eventQueue, numCrossedLines);
+			EventPoint event = eventQueue.pollFirstEntry().getValue();
+			switch (event.type) {
+				case Startpoint:
+					treatStartpoint(event, sweepLine, eventQueue);
+					break;
+				case Endpoint:
+					treatEndpoint(event, sweepLine, eventQueue);
+					break;
+				case Intersection:
+					treatIntersection(event, sweepLine, eventQueue);
+				default:
+					break;
 			}
-			eventQueue.remove(0);
 		}
 		
+		return numCrossedLines;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		long timeStart = System.currentTimeMillis();
+		int numCrossedLines = countCrossedLines(FILE1);
 		long timeEnd = System.currentTimeMillis();
 		System.out.println("Number of crossed lines: " + numCrossedLines);
+		System.out.println("Verlaufszeit der Datei: " + (timeEnd - timeStart) + " Millisek.");
+		
+
+		timeStart = System.currentTimeMillis();
+		numCrossedLines = countCrossedLines(FILE2);
+		timeEnd = System.currentTimeMillis();
+		System.out.println("\nNumber of crossed lines: " + numCrossedLines);
+		System.out.println("Verlaufszeit der Datei: " + (timeEnd - timeStart) + " Millisek.");
+		
+
+		timeStart = System.currentTimeMillis();
+		numCrossedLines = countCrossedLines(FILE3);
+		timeEnd = System.currentTimeMillis();
+		System.out.println("\nNumber of crossed lines: " + numCrossedLines);
+		System.out.println("Verlaufszeit der Datei: " + (timeEnd - timeStart) + " Millisek.");
+		
+
+		timeStart = System.currentTimeMillis();
+		numCrossedLines = countCrossedLines(FILE4);
+		timeEnd = System.currentTimeMillis();
+		System.out.println("\nNumber of crossed lines: " + numCrossedLines);
 		System.out.println("Verlaufszeit der Datei: " + (timeEnd - timeStart) + " Millisek.");
 	}
 }
